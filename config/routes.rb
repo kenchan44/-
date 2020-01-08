48f51361d84be19e7295868a247ec85f3ac22c7a
kenchan44 <19ken93@gmail.com>
2020-01-08 21:15:06 +0900

    グループ管理実装
    グループ新規登録、編集画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root 'messages#index'
+  devise_for :users
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
+
 end
